c5c553894fcc44dc6aa145b6a3ed12c4aeb8ac4f
Letania Ferreira <letaniaferreira@gmail.com>
2017-05-16 16:20:35 -0700

    second draft

--- language_learning.py
+++ language_learning.py
@@ -1,14 +1,16 @@
 print "Welcome to Language Learning" 
 print "We can go over many things today: "
-topics = ["-Greetings", "-Hotel", "-Airport", "-Restaurant", "-Exit"]
+topics = ["-Small talk", "-Hotel", "-Airport", "-Restaurant", "-Exit"]
 for variable in topics:
     print variable 
 start = raw_input("Please choose a topic above to learn more about it: ")
 if start.strip().lower() == "greetings":
     print "Lets play a guessing game."
     print " Please choose the best translation for each of the items bellow: "
-    greetings = ["oi", "tudo bem?", "como vai?", "qual o seu nome?", "tchau", "bom dia" "boa tarde", "boa noite"]
-    for item in greetings:
-        print item
+    #time = ["bom dia", "boa tarde", "boa noite"]
+    #for item in greetings:
+        #print item
+    #hi = ["oi", "tudo bem?", "como vai?", "qual o seu nome?"]
+    #tchau = ['hi', 'bye', 'man']
 
 
