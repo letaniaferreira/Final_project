18b5296abae41e38c75f69d87092bec22883466e
Letania Ferreira <letaniaferreira@gmail.com>
2017-12-08 12:25:45 -0800

    divided code further, using more functions"

--- language_learning.py
+++ language_learning.py
@@ -1,31 +1,76 @@
-from dict_file import *
 import ast
 
-def guessing_game(user_choice):
-    """takes yes or no and evaluates if user wants to play"""
-    if user_choice.lower() == "yes" or user_choice.lower() == "y":
+def keep_playing_game(play_game):
+    """evaluates if user wants to play"""
+   
+    if play_game.lower() == "yes" or play_game.lower() == "y" or play_game.lower() == "yep":
         return True
-    if user_choice.lower() == "no" or user_choice.lower() == "n":
+    elif play_game.lower() == "no" or play_game.lower() == "n" or play_game == "5":
+        print "Goodbye!"
+        return False
+    else:
+        print "Invalid input"
         return False
 
+def choice_of_topic():
+    """allows user to choose a semantic category for game"""
 
+    print "We can go over many things today: "
+    print "\n"
+    list_of_topics = ["1-Small talk", "2-Hotel", "3-Airport", "4-Restaurant", "5-Exit"]
+    for each_topic in list_of_topics:
+        print each_topic 
+    print "\n"
+    topic = raw_input("Please choose a number above to have some fun with that topic: ")
+    print "\n"
 
-def import_list(file):
+    return topic
+
+def valid_vocab_options(topic):
+    """renders valid options of vocab options"""
+
+    vocabulary_file = None
+
+    if topic == "1":
+        vocabulary_file = 'small_talk_vocab.py'
+    elif topic == "2":
+        vocabulary_file = 'hotel_vocab.py'
+    elif topic == "3":
+        vocabulary_file = 'airport_vocab.py'
+    elif topic == "4":
+        vocabulary_file = 'restaurant_vocab.py'
+
+    return vocabulary_file
+
+def invalid_vocab_options(topic):
+    """renders invalid vocab options"""
+
+    if topic == "5":
+        print "Are you sure you want to exit?"
+        quit = raw_input("Enter 1 for yes and 2 for no.")
+    if quit == "1": 
+        keep_playing_game("no")
+        vocabulary_file = None  # when keep_playing_game is called it goes to 133 and crashs
+    else:
+        vocabulary_file = None
+        print "Invalid option. Please enter a number 1 - 5" 
+        
+
+def import_vocab_file(file):
     """imports files from different topics"""
-    open_file = open(file)
+    open_file = open(file) 
 
     dict_list = []
 
     for line in open_file:
         dict_list.append(line)
 
-    open_file.close() ### is this in the correct place?
+    open_file.close() 
 
     return dict_list
 
-def looping_through_dict(lista):
+def string_to_dict(dict_line): 
 
-    dict_line = lista[0] 
     lst = ast.literal_eval(dict_line)
 
     return lst
@@ -36,12 +81,12 @@ def formulating_question(lst):
 
     
     for k, v in lst.items():
-        question = lst["Q"]
+        question = lst["question"]
 
     print "Please choose the best translation for the Portuguese word: %s" % (question)
     print "\n"
     for key, value in lst.items():
-        if key != "Q":
+        if key != "question":
             print value
     print "\n"
     answer = raw_input("> ")
@@ -52,7 +97,7 @@ def formulating_question(lst):
 def giving_feedback(lst, answer):
     """evaluates answer"""
         
-    if answer == lst["RA"]:
+    if answer == lst["right_answer"]:
         print "Well done! Let's go over the next item or press 5 to Exit"
     elif answer == '5':
         print "You asked to exit. Goodbye, hope to see you again soon!"
@@ -62,51 +107,44 @@ def giving_feedback(lst, answer):
 
 def main():
     """Greets player, allows player to choose a topic, calls formulating_question, calls giving_feedback, gives option to play another round"""
-    
-    import_list('dict_file.py')
 
     print "\n"
     print "Welcome to Language Learning" 
     print "\n"
     print "Let's test your Portuguese knowledge!"
+
     print "\n"
-    print "We can go over many things today: "
-    print "\n"
-    topics = ["1-Small talk", "2-Hotel", "3-Airport", "4-Restaurant", "5-Exit"]
-    for topic in topics:
-        print topic 
-    start = raw_input("Please choose a number above to have some fun with that topic: ")
-    print "\n"
-    if start == "1":
-    
+    play_game = raw_input("Do you want to play a round of guessing game?: ")
+    print "\n"  
 
-        #print " Please choose the best translation for each of the items bellow: "
-    #time = ["bom dia", "boa tarde", "boa noite"]
-    #for item in greetings:
-        #print item
-    #hi = ["oi", "tudo bem?", "como vai?", "qual o seu nome?"]
-        print "\n"
+    new_game_round = keep_playing_game(play_game)
 
+    index = 0
 
-        user_choice = raw_input("Do you want to play a round of guessing game?: ")
+    while new_game_round: ## and index in range(len(list_of_dictionaries)):
 
-        print "\n"
-
-    new_game_round = guessing_game(user_choice)
-
-    while new_game_round:
-
-        list_of_dictionaries = import_list('dict_file.py')
-        dict_loop = looping_through_dict(list_of_dictionaries)
-        users_answer = formulating_question(dict_loop)
-        giving_feedback(dict_loop, users_answer)
+        topic_choice = choice_of_topic()
+        if topic_choice != None:
+            vocabulary_option = valid_vocab_options(topic_choice)
+            if vocabulary_option == None:
+                invalid_vocab_options(topic_choice)
+            else:
+                vocab_file = import_vocab_file(vocabulary_option)
+            dict_index = vocab_file[index]# lines at vocab files - needs to add reference
+            dict_loop = string_to_dict(dict_index)
+            users_answer = formulating_question(dict_loop)
+            giving_feedback(dict_loop, users_answer)
+            index = index + 1
 
 
-        user_choice = raw_input("Do you want to play a new round of guessing game?: ")
+            play_game = raw_input("Do you want to play a new round of guessing game?: ")
 
-        print "\n"
+            print "\n"
 
-        new_game_round = guessing_game(user_choice)
+            new_game_round = keep_playing_game(play_game)
+
+        else:
+            topic_choice = choice_of_topic()
 
         
 
